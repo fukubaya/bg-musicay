766480666a6ece061f6f81e0f9b1890de06810f9
Yuichiro FUKUBAYASHI <fukubaya@gmail.com>
2023-08-15 01:01:57 +0900

    first version

---
+++ README.md
@@ -0,0 +1,14 @@
+# BATTEN 音楽の日 ランキング予想
+
+## setup and run
+
+```bash
+npm install
+npm run dev
+```
+
+## build
+
+```bash
+npm run build
+```
---
+++ src/App.vue
@@ -0,0 +1,340 @@
+<script lang="ts">
+import { ref, defineComponent, onMounted } from "vue";
+import imgGray from "./assets/gray.png";
+import imgYellow from "./assets/yellow.png";
+import imgBlue from "./assets/blue.png";
+import imgRed from "./assets/red.png";
+import imgPurple from "./assets/purple.png";
+import imgPink from "./assets/pink.png";
+import imgGreen from "./assets/green.png";
+
+const SONG_LIST = [
+   "OiSa",
+   "わたし、恋始めたってよ！",
+   "YOIMIYA",
+   "御祭sawagi",
+   "(6位)さがしもの",
+   "和・華・蘭",
+   "沸く星",
+   "Bright & Breezy",
+   "南風音頭",
+   "禊 the MUSIC",
+   "虹ノ湊",
+   "FREEな波に乗って",
+   "ばりかたプライド",
+   "スウィンギタイ",
+   "MILLION SUMMERS",
+   "OTOMEdeshite",
+   "でぃすたんす",
+   "Dancer in the night",
+   "ジャン！ジャン！ジャン！",
+   "ありがとーと",
+   "Dear My Blues",
+   "Just mean it!",
+   "Over",
+   "Happy",
+   "BDM",
+   "Killer Killer Smile",
+   "6STARS",
+   "崇シ増シ×××物語",
+   "己MYself",
+   "MEGRRY GO ROUND",
+   "無敵のビーナス",
+   "ハカタダンスホールベイベー",
+   "スターダスト",
+   "Number Shot",
+   "bye bye bye",
+   "(7位)おっしょい！",
+   "ばってん少女。",
+   "びびび美少女",
+   "夢のスコール",
+   "STORM!",
+   "(8位)ますとばい！",
+   "とーと",
+   "コトバテニス",
+   "(9位)乙女ノ手札",
+   "よかよかダンス",
+   "(10位)すぺしゃるでい",
+   "ころりん HAPPY FANTASY",
+   "フレッ！フレッ！フレンズ",
+   "君の手",
+   "でてこいとびきりZENKAIパワー",
+   "知っとっちゃん",
+   "さよならDESTINY",
+   "いざゆけ若鷹軍団",
+   "アツか夏きたばい！",
+   "夢のキャンバス",
+ ];
+
+ export default defineComponent({
+   setup(){
+     const songList = SONG_LIST;
+     const canvasRef = ref<HTMLCanvasElement>();
+
+     const name = ref<string>("");
+     const color = ref<string>("gray");
+     const img = ref<HTMLImageElement>(new Image());
+     const rank1 = ref<string>("");
+     const rank2 = ref<string>("");
+     const rank3 = ref<string>("");
+     const rank4 = ref<string>("");
+     const rank5 = ref<string>("");
+     
+     const clearCanvas = () => {
+       if (canvasRef.value !== undefined) {
+         const ctx = canvasRef.value.getContext("2d");
+         ctx?.clearRect(0, 0, canvasRef.value.width, canvasRef.value.height);
+       }
+     };
+     const loadImage = () => {
+       img.value.onload = () => {
+         clearCanvas();
+         if (canvasRef.value !== undefined) {
+           canvasRef.value.width = img.value.width;
+           canvasRef.value.height = img.value.height;
+         }         
+         drawImage();
+       };
+       switch(color.value){
+   case "yellow":
+     img.value.src = imgYellow;
+     break;
+   case "blue":
+     img.value.src = imgBlue;
+     break;
+   case "red":
+     img.value.src = imgRed;
+     break;
+   case "purple":
+     img.value.src = imgPurple;
+     break;
+   case "pink":
+     img.value.src = imgPink;
+     break;
+   case "green":
+     img.value.src = imgGreen;
+     break;
+   default:
+     img.value.src = imgGray;
+     break;
+       }
+
+     }
+     const drawImage = () => {
+       const ctx = canvasRef.value?.getContext("2d");
+       if (ctx == null) {
+         return;
+       }
+
+       ctx.drawImage(img.value, 0, 0);
+       
+       // name
+       ctx.font = "bold 90px sans-serif";
+       ctx.textAlign = "center";
+       ctx.textBaseline = "top";
+       ctx.fillText(name.value, 11 + (711/2) ,17, 711);
+       
+       ctx.textAlign = "left";
+       const rankWidth = 761;
+       ctx.fillText(rank1.value, 177, 139, rankWidth);
+       ctx.fillText(rank2.value, 177, 257, rankWidth);
+       ctx.fillText(rank3.value, 177, 375, rankWidth);
+       ctx.fillText(rank4.value, 177, 493, rankWidth);
+       ctx.fillText(rank5.value, 177, 612, rankWidth);
+     };
+
+     onMounted(()=>{
+      loadImage();
+    });
+
+     const downloadImage = () => {
+       if (canvasRef.value === undefined) {
+         return;
+       }
+
+       const mime = "image/png";
+       const base64 = canvasRef.value.toDataURL(mime);
+       const bin = atob(base64.split(",")[1]);
+       const buf = new Uint8Array(bin.length);
+       for (let i = 0; i < bin.length; i++) {
+         buf[i] = bin.charCodeAt(i);
+       }
+       const blob = new Blob([buf.buffer], { type: mime });
+       const data = window.URL.createObjectURL(blob);
+
+       const a = document.createElement("a");
+       a.download = "ranking.png";
+       a.href = data;
+       a.target = "_blank";
+       document.body.appendChild(a);
+       a.click();
+       a.remove();
+     };
+
+     const openTwitter = () => {
+       const texts = [
+         name.value + "のランキング予想",
+         "1位:" + rank1.value,
+         "2位:" + rank2.value,
+         "3位:" + rank3.value,
+         "4位:" + rank4.value,
+         "5位:" + rank5.value
+       ];
+       const hashtags = [
+         "ばってん少女隊",
+         "ばっしょー",
+         "BATTENGIRLS",
+         "BATTEN音楽の日"
+       ];
+
+       const text = texts.join("\n") + "\n";
+       const hashtag = hashtags.join(",");
+       const url = "https://twitter.com/intent/tweet?text=" + encodeURI(text) + "&hashtags=" + encodeURI(hashtag);
+       window.open(url, "_blnak");
+     };
+
+    return {
+      name,
+      color,
+      rank1,
+      rank2,
+      rank3,
+      rank4,
+      rank5,
+      songList,
+      canvasRef,
+      loadImage,
+      drawImage,
+      downloadImage,
+      openTwitter,
+    };
+  }   
+});
+</script>
+
+<template>
+  <v-app id="bgongaku">
+    <v-app-bar
+      density="compact"
+      color="primary"
+    >
+      <v-app-bar-title>BATTEN音楽の日 ランキング予想</v-app-bar-title>
+    </v-app-bar>
+
+    <v-main>
+      <v-container>
+        <v-row>
+          <v-col
+            xs="12"
+            sm="12"
+            md="6"
+          >
+            <canvas
+              ref="canvasRef"
+              class="result"
+            />
+          </v-col>
+          <v-col
+            xs="12"
+            sm="12"
+            md="6"
+          >
+            <v-row>
+              <v-col>
+                <v-btn-toggle
+                  v-model="color"
+                  rounded="0"
+                  group
+                  @update:model-value="loadImage"
+                >
+                  <v-btn value="gray">
+                    グレー
+                  </v-btn>
+                  <v-btn value="yellow">
+                    黄色
+                  </v-btn>
+                  <v-btn value="blue">
+                    青
+                  </v-btn>
+                  <v-btn value="red">
+                    赤
+                  </v-btn>
+                  <v-btn value="purple">
+                    紫
+                  </v-btn>
+                  <v-btn value="pink">
+                    ピンク
+                  </v-btn>
+                  <v-btn value="green">
+                    緑
+                  </v-btn>
+                </v-btn-toggle>
+              </v-col>
+
+              <v-col>
+                <v-btn
+                  icon="mdi-download"
+                  size="x-small"
+                  @click="downloadImage"
+                />
+                <v-btn
+                  icon="mdi-twitter"
+                  size="x-small"
+                  @click="openTwitter"
+                />
+              </v-col>
+            </v-row>
+  
+            <v-text-field
+              v-model="name"
+              label="なまえ"
+              @change="drawImage"
+              @keyup="drawImage"
+            />
+            <v-select
+              v-model="rank1"
+              :items="songList"
+              density="compact"
+              label="1位"
+              @update:model-value="drawImage"
+            />
+            <v-select
+              v-model="rank2"
+              :items="songList"
+              density="compact"
+              label="2位"
+              @update:model-value="drawImage"
+            />
+            <v-select
+              v-model="rank3"
+              :items="songList"
+              density="compact"
+              label="3位"
+              @update:model-value="drawImage"
+            />
+            <v-select
+              v-model="rank4"
+              :items="songList"
+              density="compact"
+              label="4位"
+              @update:model-value="drawImage"
+            />
+            <v-select
+              v-model="rank5"
+              :items="songList"
+              density="compact"
+              label="5位"
+              @update:model-value="drawImage"
+            />
+          </v-col>
+        </v-row>
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<style scoped>
+canvas.result {
+  width: 100%;
+}
+</style>
